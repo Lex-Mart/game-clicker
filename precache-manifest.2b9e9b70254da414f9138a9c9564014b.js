554643588ae916e3bc4e0ede3ab2089da229cfa7
Lex-Mart <lex3mart@gmail.com>
2021-11-11 11:36:32 +0300

    Updates

---
+++ precache-manifest.2b9e9b70254da414f9138a9c9564014b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e7fb2240ea6e7202bb50751366429f",
+    "url": "/game-clicker/index.html"
+  },
+  {
+    "revision": "39ebde004cc6cfcb5ddf",
+    "url": "/game-clicker/static/css/main.95fff283.chunk.css"
+  },
+  {
+    "revision": "dfab000014dfd6da95de",
+    "url": "/game-clicker/static/js/2.08107295.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/game-clicker/static/js/2.08107295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ebde004cc6cfcb5ddf",
+    "url": "/game-clicker/static/js/main.84f9550d.chunk.js"
+  },
+  {
+    "revision": "5d23759e59289a7d6a28",
+    "url": "/game-clicker/static/js/runtime-main.62439dcd.js"
+  }
+]);
